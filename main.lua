b2cdff5bec9543488f42e35f9b68e4e1349fa8ee
Michael Burij <michael.burij@gmail.com>
2025-02-08 21:02:03 +0100

    Additional path and cpath

--- main.lua
+++ main.lua
@@ -1,3 +1,7 @@
+package.path = "./pkgs/share/lua/5.4/?.lua;" .. package.path
+package.path = "./share/lua/5.4/?/init.lua;" .. package.path
+package.cpath = "./pkgs/lib/lua/5.4/?.so;" .. package.cpath
+
 local core = require("modules.llw-core")
 core.globalize(core)
 dofile("app.lua")
