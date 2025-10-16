5305d91946348feef4b0c54e84c33c2660535740
burij <michael.burij@gmail.com>
2025-10-16 19:13:22 +0200

    scope for the next release

--- main.lua
+++ main.lua
@@ -2,7 +2,10 @@ local core = require "modules.lua-light-wings" core.globalize(core)
 local test = require "modules.tests"
 local conf = require "conf"
 
-conf.version = "Lua Light Wings, Version 0.3.1"
+conf.version = "Lua Light Wings, Version 0.4-dev"
+-- TODO core.case: replacement for if else statements
+-- TODO core.pile: function composing
+-- TODO update documentation
 
 
 _G.debug_mode = conf.debug_mode or true
